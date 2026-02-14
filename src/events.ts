aeeadafc1ee9852c67abaa12731c778f0e338a5f
romeoscript <romeobourne211@gmail.com>
2026-02-14 10:00:00 +0100

    feat: add event listener support

---
+++ src/events.ts
@@ -0,0 +1,7 @@
+export function listenToEvents(
+  contractId: string,
+  callback: (event: any) => void,
+) {
+  // Event listener placeholder
+  console.log("Listening to events for", contractId);
+}
